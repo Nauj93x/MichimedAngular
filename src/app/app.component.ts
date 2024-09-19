0d32c560388c73a7bff7d33b9c43de1d4ac6d58c
Adrian Ruiz <adrianruiz0208@gmail.com>
2024-09-19 11:38:07 -0500

    Landing ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'MichimedAngular';
+export class AppComponent implements AfterViewInit {
+
+  ngAfterViewInit() {
+    const carousel = document.querySelector('.carousel') as HTMLElement;
+    const slides = document.querySelectorAll('.slide') as NodeListOf<HTMLElement>;
+    let currentSlide = 0;
+
+    function showSlide(n: number) {
+      currentSlide = (n + slides.length) % slides.length;
+      if (carousel) {
+        carousel.style.transform = `translateX(-${currentSlide * 100}%)`;
+      }
+    }
+
+    function nextSlide() {
+      showSlide(currentSlide + 1);
+    }
+
+    setInterval(nextSlide, 5000);
+
+    // Recargar al darle click en el logo
+    const logo = document.querySelector('.logo-landing') as HTMLElement;
+    if (logo) {
+      logo.addEventListener('click', () => {
+        location.reload();
+      });
+    }
+  }
 }

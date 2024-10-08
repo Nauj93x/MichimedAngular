8592cde811daf75f3280e05a72257293cfe1164b
Carlos Mejia <mejia.carlose@javeriana.edu.co>
2024-10-08 18:12:51 -0500

    restoring

--- src/app/pages/mascotas/mascotas.component.ts
+++ src/app/pages/mascotas/mascotas.component.ts
@@ -1,17 +1,16 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import {ConfirmationService, MessageService, SelectItem } from 'primeng/api';
+import { ConfirmationService, MessageService, SelectItem } from 'primeng/api';
 import { Mascota } from '../../model/mascota';
 import { MascotaService } from '../../services/mascota.service';
-import { Table } from 'primeng/table'
+import { Table } from 'primeng/table';
 import { Cliente } from 'src/app/model/cliente';
 import { ClienteService } from '../../services/cliente.service';
-
 
 @Component({
   selector: 'app-mascotas',
   templateUrl: './mascotas.component.html',
   styleUrls: ['./mascotas.component.css'],
-  providers: [MessageService, ConfirmationService]
+  providers: [MessageService, ConfirmationService],
 })
 export class MascotasComponent implements OnInit {
   newMascotaDialog: boolean = false;
@@ -33,15 +32,20 @@ export class MascotasComponent implements OnInit {
 
   clonedMascotas: { [s: string]: Mascota } = {};
 
-  constructor(private mascotaService: MascotaService, private messageService: MessageService, private confirmationService: ConfirmationService, private ClienteService: ClienteService) {}
+  constructor(
+    private mascotaService: MascotaService,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService,
+    private ClienteService: ClienteService
+  ) {}
 
   ngOnInit(): void {
-    this.mascotaService.getMascotas().subscribe(
-      (mascotas) => this.mascotas = mascotas
-    );
+    this.mascotaService
+      .getMascotas()
+      .subscribe((mascotas) => (this.mascotas = mascotas));
 
     this.ClienteService.getClientes().subscribe(
-      (clientes) => this.clientes = clientes
+      (clientes) => (this.clientes = clientes)
     );
 
     this.statuses = [
@@ -58,25 +62,37 @@ export class MascotasComponent implements OnInit {
       accept: () => {
         this.mascotaService.deleteMascota(id).subscribe(
           () => {
-            this.mascotas = this.mascotas.filter(mascota => mascota.id !== id);
-            this.messageService.add({ severity: 'success', summary: '¡Exitoso!', detail: 'Mascota eliminada', life: 3000 });
+            this.mascotas = this.mascotas.filter(
+              (mascota) => mascota.id !== id
+            );
+            this.messageService.add({
+              severity: 'success',
+              summary: '¡Exitoso!',
+              detail: 'Mascota eliminada',
+              life: 3000,
+            });
           },
           (error) => {
-            this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error al eliminar la mascota', life: 3000 });
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Error',
+              detail: 'Error al eliminar la mascota',
+              life: 3000,
+            });
           }
         );
-      }
+      },
     });
   }
 
   getSeverity(status: string) {
     switch (status) {
-        case 'En tratamiento':
-            return 'info';
-        case 'Tratado':
-            return 'success';
-        default:
-            return 'info';
+      case 'En tratamiento':
+        return 'info';
+      case 'Tratado':
+        return 'success';
+      default:
+        return 'info';
     }
   }
 
@@ -87,10 +103,19 @@ export class MascotasComponent implements OnInit {
   onRowEditSave(mascota: Mascota) {
     this.mascotaService.updateMascota(mascota).subscribe(
       () => {
-        this.messageService.add({ severity: 'success', summary: '¡Exitoso!', detail: 'Mascota actualizada' });
+        this.messageService.add({
+          severity: 'success',
+          summary: '¡Exitoso!',
+          detail: 'Mascota actualizada',
+        });
       },
       (error) => {
-        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error al actualizar la mascota', life: 3000 });
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'Error al actualizar la mascota',
+          life: 3000,
+        });
       }
     );
   }
@@ -101,30 +126,33 @@ export class MascotasComponent implements OnInit {
   }
 
   @ViewChild('mascotasTable') mascotasTable: Table | undefined;
-  applyFilterGlobal($event:any, stringVal:string) {
-    this.mascotasTable?.filterGlobal(($event.target as HTMLInputElement).value, stringVal);
+  applyFilterGlobal($event: any, stringVal: string) {
+    this.mascotasTable?.filterGlobal(
+      ($event.target as HTMLInputElement).value,
+      stringVal
+    );
   }
 
   openView(mascota: Mascota) {
     this.selectedMascota = mascota;
-    for (const cliente of this.clientes) {
-      if (cliente.id !== undefined){
-      this.ClienteService.getClienteMascotas(cliente.id).subscribe(
-      (clienteMascotas) => {
-        const clienteMascota = clienteMascotas.find(cm => cm.id === this.selectedMascota.id);
-        if (clienteMascota) {
-        this.selectedCliente = cliente;
-        }
-      }
-      )};
-    }
     this.viewMascotaDialog = true;
   }
 
   openNew() {
-    this.mascota = {nombre: '', edad: 0, raza: '', peso: 0, enfermedad: '', estado: '', fechaEntrada: '', fechaSalida: '', medicamento: '', foto: ''};
+    this.mascota = {
+      nombre: '',
+      edad: 0,
+      raza: '',
+      peso: 0,
+      enfermedad: '',
+      estado: '',
+      fechaEntrada: '',
+      fechaSalida: '',
+      medicamento: '',
+      foto: '',
+    };
     this.submitted = false;
-    this.clientes = this.clientes
+    this.clientes = this.clientes;
     this.newMascotaDialog = true;
   }
 
@@ -137,29 +165,18 @@ export class MascotasComponent implements OnInit {
     this.submitted = true;
 
     if (this.mascota.nombre?.trim() && this.dueno) {
-      this.mascotaService.addMascota(this.mascota).subscribe(
-        (response: any) => {
-          this.messageService.add({ severity: 'success', summary: '¡Exitoso!', detail: 'Mascota creada', life: 3000 });
+      this.mascotaService
+        .addMascota(this.mascota)
+        .subscribe((response: any) => {
+          this.messageService.add({
+            severity: 'success',
+            summary: '¡Exitoso!',
+            detail: 'Mascota creada',
+            life: 3000,
+          });
           this.mascotas.push(this.mascota);
           this.newMascotaDialog = false;
-
-          if (this.dueno.id !== undefined) {
-            this.ClienteService.addMascota(this.dueno.id, this.mascota).subscribe(
-              (response) => {
-                this.messageService.add({ severity: 'success', summary: '¡Exitoso!', detail: 'Mascota asignada al dueño', life: 3000 });
-              },
-              (error) => {
-                this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error al asignar la mascota al dueño', life: 3000 });
-              }
-            );
-          }
-        },
-        (error) => {
-          this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error al crear la mascota', life: 3000 });
-        }
-      );
-    } else {
-      this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Por favor, complete el formulario correctamente.', life: 3000 });
+        });
     }
   }
 
--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Cliente } from '../model/cliente';  // Interfaz Cliente
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Mascota } from '../model/mascota';
 
 @Injectable({
   providedIn: 'root'
@@ -22,15 +21,6 @@ export class ClienteService {
   // Método para obtener un cliente por su id
   getClienteById(id: number): Observable<Cliente> {
     return this.http.get<Cliente>('http://localhost:8090/clientes/' + id);
-  }
-
-  getClienteMascotas(id:number): Observable<Mascota[]> {
-    return this.http.get<Mascota[]>('http://localhost:8090/clientes/mascotas/' + id);
-  }
-
-  //Metodo para relacionar una Mascota
-  addMascota(id: number, mascota:Mascota){
-    return this.http.post('http://localhost:8090/clientes/' + id + '/mascotas', mascota);
   }
 
   // Método para agregar una nueva cliente

915fca56d352299ef442c1752815736fa794b7c7
Carlos Mejia <mejia.carlose@javeriana.edu.co>
2024-10-17 09:33:21 -0500

    Disponibilidad drogas

--- src/app/components/drogas-aux-table/drogas-aux-table.component.ts
+++ src/app/components/drogas-aux-table/drogas-aux-table.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { Table } from 'primeng/table';
+import { Component, OnInit } from '@angular/core';
 import { Droga } from 'src/app/model/droga';
 import { DrogaService } from 'src/app/services/drogas.service';
 
@@ -8,76 +7,38 @@ import { DrogaService } from 'src/app/services/drogas.service';
   templateUrl: './drogas-aux-table.component.html',
   styleUrls: ['./drogas-aux-table.component.css']
 })
-export class DrogasAuxTableComponent {
+export class DrogasAuxTableComponent implements OnInit {
   drogas: Droga[] = [];
-  errorMessage: string = '';
-  @ViewChild('dt') table!: Table; // Referencia a la tabla de PrimeNG
 
   constructor(private drogaService: DrogaService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.drogaService.getDrogas().subscribe((data) => {
       this.drogas = data;
     });
   }
 
-  // Método para cargar medicamentos desde un archivo Excel
-  cargarMedicamentos(event: any): void {
-    const file = event.target.files[0];
-
-    if (!file) {
-      this.errorMessage = 'Por favor, sube un archivo válido.';
-      return;
+  getDisponibilidad(uniDisp: number): string {
+    if (uniDisp <= 2) {
+      return 'Reabastecer';
+    } else if (uniDisp < 5) {
+      return 'Escaso';
+    } else if (uniDisp >= 8) {
+      return 'Abundante';
+    } else {
+      return 'Normal';
     }
-
-    this.drogaService.cargarMedicamentos(file).subscribe(
-      (data: any) => {
-        // Verifica si ya hay datos en la tabla y solicita confirmación antes de sobrescribir
-        if (this.drogas.length > 0) {
-          const confirmOverwrite = confirm('Ya hay datos cargados, ¿quieres agregar solo las drogas nuevas o sobrescribirlas?');
-
-          if (confirmOverwrite) {
-            // this.agregarDrogasUnicas(data); // Agregar solo las drogas nuevas
-            this.drogas = data;
-          } else {
-            this.drogas = data; // Sobrescribir con los nuevos datos
-          }
-        } else {
-          this.drogas = data;
-        }
-
-        this.drogaService.addDrogas(this.drogas); // Guardar los datos en el servidor
-        // this.guardarDrogasEnLocalStorage();
-        this.errorMessage = '';
-      },
-      (error) => {
-        this.errorMessage = 'Error al procesar el archivo. Asegúrate de que el formato sea correcto.';
-      }
-    );
   }
 
-  // Método para guardar las drogas en localStorage
-  guardarDrogasEnLocalStorage(): void {
-    localStorage.setItem('drogas', JSON.stringify(this.drogas));
-  }
-
-  // Método para agregar solo las drogas nuevas
-  agregarDrogasUnicas(nuevasDrogas: any[]): void {
-    const drogasExistentesNombres = this.drogas.map(droga => droga.nombre);
-
-    nuevasDrogas.forEach(drogaNueva => {
-      if (!drogasExistentesNombres.includes(drogaNueva.nombre)) {
-        this.drogas.push(drogaNueva); // Agregar solo las drogas nuevas
-      }
-    });
-
-    this.guardarDrogasEnLocalStorage(); // Actualiza el localStorage con los nuevos datos
-  }
-
-  // **Limpiar datos de la tabla y localStorage**
-  clear(dt: Table) {
-    dt.clear(); // Limpiar los filtros de la tabla (esto ya funcionaba)
-    this.drogas = []; // Limpiar los datos de la tabla
-    localStorage.removeItem('drogas'); // Limpiar los datos del localStorage
+  getDisponibilidadClass(uniDisp: number): string {
+    if (uniDisp <= 2) {
+      return 'reabastecer';
+    } else if (uniDisp < 5) {
+      return 'escaso';
+    } else if (uniDisp >= 8) {
+      return 'abundante';
+    } else {
+      return 'normal';
+    }
   }
 }
